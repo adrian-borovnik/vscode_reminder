8c69321d8d591564bc972fd494884420aa6742b6
adrian-borovnik <adrian.borovnik@gmail.com>
2023-08-30 20:48:13 +0200

    feat: :sparkles: Add merge funcitonality

--- src/extension.ts
+++ src/extension.ts
@@ -23,9 +23,11 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInputBox({ prompt: 'Enter the new interval duration in minutes' }).then((value) => {
 			if (value) {
 				minutes = parseInt(value)
+				if (minutes < 1) minutes = 1
 				clearInterval(interval)
 				interval = initInterval(minutes)
-				vscode.window.showInformationMessage('Interval duration changed to ' + minutes + ' minutes.')
+				let message = 'Interval duration changed to ' + (minutes <= 1 ? minutes + ' minute.' : minutes + ' minutes.')
+				vscode.window.showInformationMessage(message)
 			}
 		})
 	})
@@ -36,17 +38,15 @@ export function activate(context: vscode.ExtensionContext) {
 
 function initInterval(minutes: number = 30) {
 	return setInterval(() => {
-		const date = new Date()
-		const time = date.getHours() + ':' + date.getMinutes()
-
 		const messages = ["Don't forget to merge with the develop branch...", "It's time to merge with the develop branch.", 'Develop branch waits to be merged.', 'Can you even merge develop bro?', 'Imagine not merging with the develop branch...', 'Only a fool would not merge the develop branch...', "You're not gonna merge with the develop branch, are you?"]
 
 		const message = messages[Math.floor(Math.random() * messages.length)]
 
 		vscode.window.showInformationMessage(message, 'Merge', 'Cancel').then((value) => {
 			if (value === 'Merge') {
-				// run git merge origin/develop in the console
-				vscode.window.showInformationMessage('Merging...')
+				const terminal = vscode.window.createTerminal(`Merge terminal`)
+				terminal.sendText('git merge origin/develop')
+				terminal.dispose()
 			}
 		})
 	}, 1000 * 60 * minutes)
